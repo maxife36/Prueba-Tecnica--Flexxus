87adae512d7ae107a5dbbab210b90de9e52f919a
maxife36 <maxife36@gmail.com>
2024-10-31 15:33:15 -0300

    agrgeo dtos para servicios de account

---
+++ RestApi/account/src/dtos/user.dto.ts
@@ -0,0 +1,51 @@
+import { IsNotEmpty, IsOptional, IsString, Length } from "class-validator";
+import { plainToClass } from "class-transformer";
+
+export class UserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 50)
+  username!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 50) 
+  password!: string;
+
+  static fromPlain(plain: object): UserDto {
+    return plainToClass(UserDto, plain);
+  }
+}
+
+export class LoginUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(1, 50)
+  username!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(6, 50) 
+  password!: string;
+  
+  static fromPlain(plain: object): LoginUserDto {
+    return plainToClass(LoginUserDto, plain);
+  }
+}
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(6, 50)
+  password?: string;
+
+  static fromPlain(plain: object): UpdateUserDto {
+    return plainToClass(UpdateUserDto, plain);
+  }
+}
+

ec2515377c818022f15e9f83978e4676f719daaf
maxife36 <maxife36@gmail.com>
2024-10-31 15:17:36 -0300

    modificacione para implementacion del servicio de auth

--- RestApi/database/main.ts
+++ RestApi/database/main.ts
@@ -1,19 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 import ArticleRoutes from "./src/routes/article.routes";
 import userRoutes from "./src/routes/user.route";
 import errorHandler from "./src/handlers/errorHandler";
+import ServiceAuthMiddleware from "./src/auth/middlewares/serviceAuth.middleware";
+import LoggedAuth from "./src/auth/middlewares/userLogged.middleware";
 
 dotenv.config();
 
 const PORT = process.env.DATABASE_CONTAINER_PORT ?? 3003;
 
+const AuthService = new ServiceAuthMiddleware("database", ["account"])
+const accountAuth = AuthService.authMiddlewares.account
+
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser());
 
-app.use("/api/articles", ArticleRoutes);
-app.use("/api/users", userRoutes);
+
+app.use("/api/articles", LoggedAuth,  ArticleRoutes);
+app.use("/api/users", accountAuth ,userRoutes);
+
+app.use(AuthService.serviceSignature)
 
 app.use(errorHandler);
 
